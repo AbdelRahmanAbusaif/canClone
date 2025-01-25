3c78ee0d72eea7ad40052c234dcecc118488021e
MahmoudNaser03 <alnasermahmoud0@gmail.com>
2025-01-25 18:00:49 +0300

    <edit> complete lower panel

--- Match3Clone/Assets/Scripts/Frontend/PageTransition.cs
+++ Match3Clone/Assets/Scripts/Frontend/PageTransition.cs
@@ -3,88 +3,96 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PageTransition : MonoBehaviour
-{
-    [SerializeField] private List<GameObject> pages; 
-    [SerializeField] private List<Image> imagesBorder;
-    //holding the buttons
-    [SerializeField] private List<GameObject> holder;
+
+using GameVanilla.Core;
+using GameVanilla.Game.Common;
+using GameVanilla.Game.UI;
 
 
-    [SerializeField] private Sprite ActiveBorder;
-    [SerializeField] private Sprite InactiveBorder;
-
-    [SerializeField] private GameObject rightRespawn;  
-    [SerializeField] private GameObject leftRespawn; 
-    [SerializeField] private float transitionDuration = 0.1f;
-
-    //because first page index is 1(Story page)
-    private int currentPageIndex = 1;
-    private bool isTransitioning = false;
-
-    Vector2 originSize;
-
-    public void Start()
+    public class PageTransition : MonoBehaviour
     {
-        //save the origin size of buttons
-      originSize = holder[0].GetComponent<RectTransform>().sizeDelta ;
+        [SerializeField] private List<GameObject> pages;
+        [SerializeField] private List<Image> imagesBorder;
+        //holding the buttons
+        [SerializeField] private List<GameObject> holder;
 
-        UpdateBorder(1);  
-    }
 
-    public void TransitionToPage(int targetPageIndex)
-    {
-        if (targetPageIndex < 0 || targetPageIndex >= pages.Count || isTransitioning || currentPageIndex == targetPageIndex)
+        [SerializeField] private Sprite ActiveBorder;
+        [SerializeField] private Sprite InactiveBorder;
+
+        [SerializeField] private GameObject rightRespawn;
+        [SerializeField] private GameObject leftRespawn;
+        [SerializeField] private float transitionDuration = 0.1f;
+
+        //because first page index is 1(Story page)
+        private int currentPageIndex = 1;
+        private bool isTransitioning = false;
+
+        Vector2 originSize;
+
+        public void Start()
         {
-            return;
+            //save the origin size of buttons
+            originSize = holder[0].GetComponent<RectTransform>().sizeDelta;
+
+            UpdateBorder(1);
         }
 
-        isTransitioning = true;
-        
-        Vector3 targetPosition = targetPageIndex > currentPageIndex ? leftRespawn.transform.position : rightRespawn.transform.position;
-        Vector3 entryPosition = targetPageIndex > currentPageIndex ? rightRespawn.transform.position : leftRespawn.transform.position;
-
-        // Activate the target page at the correct entry position
-        pages[targetPageIndex].transform.position = entryPosition;
-        pages[targetPageIndex].SetActive(true);
-
-        UpdateBorder(targetPageIndex);
-
-        // Perform the transition animation
-        DOTween.Sequence()
-            .Append(pages[currentPageIndex].transform.DOMove(targetPosition, transitionDuration))
-            .Join(pages[targetPageIndex].transform.DOMove(Vector3.zero, transitionDuration))
-            .OnComplete(() =>
+        public void TransitionToPage(int targetPageIndex)
+        {
+            if (targetPageIndex < 0 || targetPageIndex >= pages.Count || isTransitioning || currentPageIndex == targetPageIndex)
             {
-                FinalizeTransition(targetPageIndex);
-            });
-    }
+                return;
+            }
 
-    private void UpdateBorder(int targetPageIndex)
-    {
-        for (int i = 0; i < imagesBorder.Count; i++)
-        {
-            imagesBorder[i].sprite = InactiveBorder;
-            holder[i].GetComponent<RectTransform>().sizeDelta = new Vector2(originSize.x, originSize.y);
-            //enable txt
-            holder[i].GetComponent<RectTransform>().GetChild(2).gameObject.SetActive(true);
-            
-        }
-        imagesBorder[targetPageIndex].sprite = ActiveBorder;
-        holder[targetPageIndex].GetComponent<RectTransform>().sizeDelta *= new Vector2((float)1.5,(float)1.5);
-        //disable txt
-        holder[targetPageIndex].GetComponent<RectTransform>().GetChild(2).gameObject.SetActive(false);
-        
-    }
+            isTransitioning = true;
+            GetComponent<PlaySound>().Play("PopupCloseButton");
 
-    private void FinalizeTransition(int targetPageIndex)
-    {
-        for (int i = 0; i < pages.Count; i++)
-        {
-            pages[i].SetActive(i == targetPageIndex);
+            Vector3 targetPosition = targetPageIndex > currentPageIndex ? leftRespawn.transform.position : rightRespawn.transform.position;
+            Vector3 entryPosition = targetPageIndex > currentPageIndex ? rightRespawn.transform.position : leftRespawn.transform.position;
+
+            // Activate the target page at the correct entry position
+            pages[targetPageIndex].transform.position = entryPosition;
+            pages[targetPageIndex].SetActive(true);
+
+            UpdateBorder(targetPageIndex);
+
+            // Perform the transition animation
+            DOTween.Sequence()
+                .Append(pages[currentPageIndex].transform.DOMove(targetPosition, transitionDuration))
+                .Join(pages[targetPageIndex].transform.DOMove(Vector3.zero, transitionDuration))
+                .OnComplete(() =>
+                {
+                    FinalizeTransition(targetPageIndex);
+                });
         }
 
-        currentPageIndex = targetPageIndex;
-        isTransitioning = false;
+        private void UpdateBorder(int targetPageIndex)
+        {
+            for (int i = 0; i < imagesBorder.Count; i++)
+            {
+                imagesBorder[i].sprite = InactiveBorder;
+                holder[i].GetComponent<RectTransform>().sizeDelta = new Vector2(originSize.x, originSize.y);
+                //enable txt
+                holder[i].GetComponent<RectTransform>().GetChild(2).gameObject.SetActive(true);
+
+            }
+            imagesBorder[targetPageIndex].sprite = ActiveBorder;
+            holder[targetPageIndex].GetComponent<RectTransform>().sizeDelta *= new Vector2((float)1.5, (float)1.5);
+            //disable txt
+            holder[targetPageIndex].GetComponent<RectTransform>().GetChild(2).gameObject.SetActive(false);
+
+        }
+
+        private void FinalizeTransition(int targetPageIndex)
+        {
+            for (int i = 0; i < pages.Count; i++)
+            {
+                pages[i].SetActive(i == targetPageIndex);
+            }
+
+            currentPageIndex = targetPageIndex;
+            isTransitioning = false;
+        }
     }
-}
+
